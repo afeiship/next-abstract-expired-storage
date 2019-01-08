7795725fe1527a35164f3cc0b7079c9d230ccb5b
afeiship <afeiship@gmail.com>
2019-01-08 23:51:08 +0800

    feat: done init

--- test/test.js
+++ test/test.js
@@ -1,9 +1,16 @@
 var nx = require('next-js-core2');
 var NxExpiredStorage = require('../src/next-expired-storage');
 
-test('json/urlencoded', function() {
-  var data = {
-    key: 1,
-    value: 2
-  };
+test('nx.ExpiredStorage', function(done) {
+  var exStorage = new NxExpiredStorage();
+  exStorage.set('test1', 'value1', 1);
+  exStorage.set('test2', 'value23123', 1);
+
+  console.log(exStorage.get('test1'), exStorage.gets());
+
+  setTimeout(() => {
+    // console.log(exStorage.get('test1'), exStorage.engine.gets());
+    // console.log(exStorage.clear('test1'), exStorage.gets());
+    done();
+  }, 2 * 1e3);
 });
